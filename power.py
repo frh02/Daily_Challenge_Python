88664760b83d915581c74ce9f786c505f0baade5
Farhan Rahman <65035083+frh02@users.noreply.github.com>
2021-03-03 18:59:19 -0500

    Create power.py

---
+++ power.py
@@ -0,0 +1,9 @@
+'''So far, we have only heard of Python's powers. Now, we will witness them!
+
+Powers or exponents in Python can be calculated using the built-in power function. Call the power function  as shown below:
+'''
+a = int(input())
+b = int(input())
+m = int(input())
+print(pow(a,b))
+print(pow(a,b,m))
