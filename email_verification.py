e0258bbde772b0756cbb18ad22de84d4bd25f579
Farhan Rahman <65035083+frh02@users.noreply.github.com>
2021-03-03 12:25:26 -0500

    Update email_verification.py

--- email_verification.py
+++ email_verification.py
@@ -1,10 +1,11 @@
-/*A valid email address meets the following criteria:
+'''A valid email address meets the following criteria:
 
 It's composed of a username, domain name, and extension assembled in this format: username@domain.extension
 The username starts with an English alphabetical character, and any subsequent characters consist of one or more of the following: alphanumeric characters, -,., and _.
 The domain and extension contain only English alphabetical characters.
 The extension is , , or  characters in length.
-Given  pairs of names and email addresses as input, print each name and email address pair having a valid email address on a new line.*/
+Given  pairs of names and email addresses as input, print each name and email address pair having a valid email address on a new line.
+'''
 
 import email.utils
 import re
