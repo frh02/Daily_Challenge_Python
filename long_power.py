68b9cfcda384f77dc9fd048f815bf5daad89ef74
Farhan Rahman <65035083+frh02@users.noreply.github.com>
2021-03-03 19:09:47 -0500

    Create long_power.py

---
+++ long_power.py
@@ -0,0 +1,15 @@
+'''Integers in Python can be as big as the bytes in your machine's memory. There is no limit in size as there is:  (c++ int) or  (C++ long long int).
+
+As we know, the result of  grows really fast with increasing .
+
+Let's do some calculations on very large integers.
+'''
+
+
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+a= int(input())
+b= int(input())
+c= int(input())
+d= int(input())
+print(pow(a,b)+pow(c,d))
