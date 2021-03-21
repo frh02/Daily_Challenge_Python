91f79db540335b91a565c92e8dbeae0d0f9dc665
Farhan Rahman <65035083+frh02@users.noreply.github.com>
2021-03-21 10:29:29 -0400

    Create intersection_set.py

---
+++ intersection_set.py
@@ -0,0 +1,17 @@
+#QUESTION:
+
+
+'''
+.intersection()
+The .intersection() operator returns the intersection of a set and the set of elements in an iterable.
+Sometimes, the & operator is used in place of the .intersection() operator, but it only operates on the set of elements in set.
+The set is immutable to the .intersection() operation (or & operation).
+'''
+
+
+
+#CODE:
+  
+_, a = input(), set(input().split())
+_, b = input(), set(input().split())
+print(len(a.intersection(b)))
