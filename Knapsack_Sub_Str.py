54c6f48efa1fc800483429917605d4f36a63b427
Farhan Rahman <65035083+frh02@users.noreply.github.com>
2021-04-02 17:05:34 -0400

    Create Knapsack_Sub_Str.py

---
+++ Knapsack_Sub_Str.py
@@ -0,0 +1,29 @@
+#QUESTION
+
+'''
+You are given an array of binary strings strs and two integers m and n.
+
+Return the size of the largest subset of strs such that there are at most m 0's and n 1's in the subset.
+
+A set x is a subset of a set y if all elements of x are also elements of y.
+
+ '''
+
+#SOLUTION
+
+class Solution(object):
+    def findMaxForm(self, strs, m, n):
+        """
+        :type strs: List[str]
+        :type m: int
+        :type n: int
+        :rtype: int
+        """
+        dp = [[0 for _ in range(n+1)] for _ in range(m+1)]
+        for str in strs:
+            zeros = str.count("0")
+            ones = len(str) - zeros
+            for i in range(m, zeros - 1, -1):
+                for j in range(n, ones - 1, -1):
+                    dp[i][j] = max(dp[i][j], dp[i-zeros][j-ones] + 1)
+        return dp[m][n]
