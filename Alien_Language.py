6b07775e19549c769d6f6a1b812c3fea89d73f6f
Farhan Rahman <65035083+frh02@users.noreply.github.com>
2021-04-09 12:16:37 -0400

    Create Alien_Language.py

---
+++ Alien_Language.py
@@ -0,0 +1,33 @@
+'''
+In an alien language, surprisingly they also use english lowercase letters, but possibly in a different order. The order of the alphabet is some permutation of lowercase letters.
+
+Given a sequence of words written in the alien language, and the order of the alphabet, return true if and only if the given words are sorted lexicographicaly in this alien language.
+
+'''
+#SOLUTION:
+
+class Solution(object):
+    def isAlienSorted(self, words, order):
+        """
+        :type words: List[str]
+        :type order: str
+        :rtype: bool
+        """
+        order_map = {}
+        for index, val in enumerate(order):
+            order_map[val] = index
+
+        for i in range(len(words) - 1):
+
+            for j in range(len(words[i])):
+                # If we do not find a mismatch letter between words[i] and words[i + 1],
+                # we need to examine the case when words are like ("apple", "app").
+                if j >= len(words[i + 1]): return False
+
+                if words[i][j] != words[i + 1][j]:
+                    if order_map[words[i][j]] > order_map[words[i + 1][j]]: return False
+                    # if we find the first different character and they are sorted,
+                    # then there's no need to check remaining letters
+                    break
+
+        return True
