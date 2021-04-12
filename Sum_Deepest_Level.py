7a44d1127da6ff6ca7bcabf32d61c3adb21fd956
Farhan Rahman <65035083+frh02@users.noreply.github.com>
2021-04-11 23:00:43 -0400

    Update Sum_Deepest_Level.py

--- Sum_Deepest_Level.py
+++ Sum_Deepest_Level.py
@@ -14,16 +14,17 @@ class Solution(object):
         :type root: TreeNode
         :rtype: int
         """
+    #using depth first search find the node with the deepest level 
         def dfs1(node):
             if not node: return 0
             return max(dfs1(node.left), dfs1(node.right)) + 1
-        
+   #define a method which will check if the current node is the deepest level or not 
         def dfs2(node, d):
             if not node: return
             if d == depth: self.ans += node.val
             dfs2(node.left, d+1)
             dfs2(node.right, d+1)
-        
+     
         self.ans = 0
         depth = dfs1(root)
         dfs2(root, 1)
