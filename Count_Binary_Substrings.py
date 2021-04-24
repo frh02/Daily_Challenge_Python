87175955a1f7b5ea0d743802a1f7d1676062ba75
Farhan Rahman <65035083+frh02@users.noreply.github.com>
2021-04-23 22:38:31 -0400

    Create Count_Binary_Substrings.py

---
+++ Count_Binary_Substrings.py
@@ -0,0 +1,24 @@
+'''
+Give a string s, count the number of non-empty (contiguous) substrings that have the same number of 0's and 1's, and all the 0's and all the 1's in these substrings are grouped consecutively.
+
+Substrings that occur multiple times are counted the number of times they occur.
+
+'''
+#solution:
+class Solution(object):
+    def countBinarySubstrings(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        groups = [1]
+        for i in xrange(1, len(s)):
+            if s[i-1] != s[i]:
+                groups.append(1)
+            else:
+                groups[-1] += 1
+
+        ans = 0
+        for i in xrange(1, len(groups)):
+            ans += min(groups[i-1], groups[i])
+        return ans
