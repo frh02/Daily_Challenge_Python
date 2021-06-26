7d0c9d3d3f4e876b17dae13f442cce1ffbf5bdfa
Farhan Rahman <65035083+frh02@users.noreply.github.com>
2021-06-26 14:24:44 -0400

    Create Hello_World.java

---
+++ Hello_World.java
@@ -0,0 +1,11 @@
+/* Welcome to the world of Java! In this challenge, we practice printing to stdout */
+
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Print output to STDOUT. Your class should be named Solution. */
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
